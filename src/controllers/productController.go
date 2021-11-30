beafba7e734badf71833f5b2243920a0119d6301
saskaradit <saskaradit@gmail.com>
2021-11-30 11:02:31 +0700

    products

---
+++ src/controllers/productController.go
@@ -0,0 +1,85 @@
+package controllers
+
+import (
+	"ambassador/src/database"
+	"ambassador/src/models"
+	"net/http"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Products(c *fiber.Ctx) error {
+	var products []models.Product
+
+	database.DB.Find(&products)
+
+	return c.JSON(products)
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	var product models.Product
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	if product.Title == "" || product.Price == 0 || product.Description == "" {
+		c.Status(http.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "please input all the fields",
+		})
+	}
+
+	database.DB.Create(&product)
+
+	return c.JSON(product)
+}
+
+func GetProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	var product models.Product
+
+	product.Id = uint(id)
+
+	database.DB.Find(&product)
+
+	return c.JSON(product)
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	var product models.Product
+
+	product = models.Product{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	if product.Title == "" || product.Price == 0 || product.Description == "" {
+		c.Status(http.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "please input all the fields",
+		})
+	}
+
+	database.DB.Model(&product).Updates(&product)
+
+	return c.JSON(product)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	var product models.Product
+
+	product = models.Product{
+		Id: uint(id),
+	}
+
+	database.DB.Delete(&product)
+
+	return nil
+}
