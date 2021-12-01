012c2302001780f5b6c6c08fc4e4ef33e5b4c94a
saskaradit <saskaradit@gmail.com>
2021-12-01 20:48:50 +0700

    checkout

--- src/controllers/orderController.go
+++ src/controllers/orderController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,4 +19,88 @@ func Orders(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(orders)
+}
+
+type CreateOrderRequest struct {
+	Code      string
+	FirstName string
+	LastName  string
+	Email     string
+	Address   string
+	Country   string
+	City      string
+	Zip       string
+	Products  []map[string]int
+}
+
+func CreateOrder(c *fiber.Ctx) error {
+	var request CreateOrderRequest
+
+	if err := c.BodyParser(&request); err != nil {
+		return err
+	}
+
+	link := models.Link{
+		Code: request.Code,
+	}
+
+	database.DB.Preload("User").First(&link)
+
+	if link.Id == 0 {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Invalid Link!",
+		})
+	}
+
+	order := models.Order{
+		Code:            link.Code,
+		UserId:          link.UserId,
+		AmbassadorEmail: link.User.Email,
+		FirstName:       request.FirstName,
+		LastName:        request.LastName,
+		Email:           request.Email,
+		Address:         request.Address,
+		Country:         request.Country,
+		City:            request.City,
+		Zip:             request.Zip,
+	}
+
+	tx := database.DB.Begin()
+	if err := tx.Create(&order).Error; err != nil {
+		tx.Rollback()
+		c.Status(http.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	for _, prod := range request.Products {
+		product := models.Product{}
+		product.Id = uint(prod["product_id"])
+		database.DB.First(&product)
+
+		total := product.Price * float64(prod["quantity"])
+
+		item := models.OrderItem{
+			OrderId:           order.Id,
+			ProductTitle:      product.Title,
+			Price:             product.Price,
+			Quantity:          uint(prod["quantity"]),
+			AmbassadorRevenue: 0.1 * total,
+			AdminRevenue:      0.9 * total,
+		}
+
+		if err := tx.Create(&item).Error; err != nil {
+			tx.Rollback()
+			c.Status(http.StatusBadRequest)
+			return c.JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+	}
+
+	tx.Commit()
+
+	return c.JSON(order)
 }
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -42,4 +42,8 @@ func Setup(app *fiber.App) {
 	ambassadorAuthenticated.Post("links", controllers.CreateLink)
 	ambassadorAuthenticated.Get("stats", controllers.Stats)
 	ambassadorAuthenticated.Get("ranks", controllers.Rankings)
+
+	checkout := api.Group("checkout")
+	checkout.Get("links/:code", controllers.GetLink)
+	checkout.Post("orders", controllers.CreateOrder)
 }
