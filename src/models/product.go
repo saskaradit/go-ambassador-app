77412f846411f1f63fe9defa3a75342a0636b6ed
saskaradit <saskaradit@gmail.com>
2021-12-01 21:29:15 +0700

    test

--- src/models/product.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Product struct {
-	Model
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Image       string  `json:"image"`
-	Price       float64 `json:"price"`
-}
